2a90c784ec9c9064f0de2014b7a2be98bf32b5dd
hatellezp <hatellezp@gmail.com>
2024-08-21 08:24:18 +0000

    [FIX] docker compose pointing to specific home

--- run_and_build_docker_file.sh
+++ run_and_build_docker_file.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 
-RUNTIME=$1 USER_ID=$(id -u) GROUP_ID=$(id -g) docker compose up -d --build
+HOME=$HOME RUNTIME=$1 USER_ID=$(id -u) GROUP_ID=$(id -g) docker compose up -d --build
