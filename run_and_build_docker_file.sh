06e9395f92baebd4fb202ed3027dc24ed46e39de
hatellezp <hatellezp@gmail.com>
2024-08-21 10:54:31 +0200

    [IMP] now we know what the command is outputting

--- run_and_build_docker_file.sh
+++ run_and_build_docker_file.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
 
+echo "HOME=$HOME RUNTIME=$1 USER_ID=$(id -u) GROUP_ID=$(id -g) docker compose up -d --build"
 HOME=$HOME RUNTIME=$1 USER_ID=$(id -u) GROUP_ID=$(id -g) docker compose up -d --build
+
