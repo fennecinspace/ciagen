f8fcab5743402311acc35808c26b5006e9157614
Mohamed Benkedadra <fennecinspace@gmail.com>
2026-04-24 11:13:48 +0200

    [REFACTOR] cleaned none useful files and removed pyfeat

--- ciagen/feature_extractors/__init__.py
+++ ciagen/feature_extractors/__init__.py
@@ -1,9 +1,7 @@
-from .auc_extractor import AUFE, au_transform
 from .vit_extractor import VitFE, vit_transform
 from .inception_extractor import InceptionFE, inception_transform
 
 AVAILABLE_FEATURE_EXTRACTORS = (
-    "au",
     "vit",
     "inception",
 )
@@ -19,14 +17,10 @@ def instance_transform(feature_extractor_name: str, **kwargs):
         return vit_transform()
     elif feature_extractor_name == "inception":
         return inception_transform(to_tensor=True)
-        # return inception_transform()
-    elif feature_extractor_name == "au":
-        return au_transform()
 
 
 def available_feature_extractors():
     return {
-        "au": AUFE,
         "vit": VitFE,
         "inception": InceptionFE,
     }
@@ -38,9 +32,7 @@ def instance_feature_extractor(feature_extractor_name: str, **kwargs):
             f"Unknown feature extractor: {feature_extractor_name}. Please verify your configuration file."
         )
 
-    if feature_extractor_name == "au":
-        return AUFE(**kwargs)
-    elif feature_extractor_name == "vit":
+    if feature_extractor_name == "vit":
         return VitFE(**kwargs)
     elif feature_extractor_name == "inception":
         return InceptionFE(**kwargs)
